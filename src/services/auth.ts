ffc192a45ba311a4c71c3bca994be6256af5738e
chaerla <chelgchen@gmail.com>
2023-10-26 00:11:04 +0700

    feat: add auth service

---
+++ src/services/auth.ts
@@ -0,0 +1,37 @@
+import { api } from '@/utils/api.ts';
+import { BaseResponse, User } from '@/utils/interfaces.ts';
+import { setToken } from '@/utils/token-storage.ts';
+
+interface LoginPayload {
+  username: string;
+  password: string;
+}
+
+interface RegisterPayload extends LoginPayload {
+  firstName: string;
+  lastName?: string;
+  email: string;
+}
+
+interface AuthResponse extends BaseResponse {
+  data: {
+    auth: {
+      user: User;
+      access_token: string;
+    };
+  };
+}
+
+export const login = async (payload: LoginPayload): Promise<User> => {
+  const response = await api.post('/auth/login', payload);
+  const res = response.data as AuthResponse;
+  setToken(res.data.auth.access_token);
+  return res.data.auth.user;
+};
+
+export const register = async (payload: RegisterPayload): Promise<User> => {
+  const response = await api.post('/auth/register', payload);
+  const res = response.data as AuthResponse;
+  setToken(res.data.auth.access_token);
+  return res.data.auth.user;
+};
