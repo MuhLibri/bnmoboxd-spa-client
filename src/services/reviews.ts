a820ab90acf91de482fd2d8b62fce193bf57967c
chaerla <chelgchen@gmail.com>
2023-11-01 23:12:30 +0700

    feat: add reviews service

---
+++ src/services/reviews.ts
@@ -0,0 +1,38 @@
+import { api } from '@/utils/api.ts';
+import { BaseResponse, CuratorReview } from '@/utils/interfaces.ts';
+
+export interface CreatedCuratorReview extends CuratorReview {
+  id: number;
+}
+interface CuratorReviewsResponse extends BaseResponse {
+  data: {
+    reviews: CreatedCuratorReview[];
+    count: number;
+  };
+}
+interface CuratorReviewResponse {
+  data: CreatedCuratorReview;
+}
+type PostCuratorReviewPayload = CuratorReview;
+type PutCuratorReviewPayload = CuratorReview;
+
+const path = '/curator-review';
+export const getCuratorReviews = async ({ page, take }: { page: number; take: number }) => {
+  const res = (await api.get(`${path}?page=${page}&take=${take}`)).data as CuratorReviewsResponse;
+  return res.data;
+};
+
+export const postCuratorReview = async (payload: PostCuratorReviewPayload) => {
+  const res = (await api.post(`${path}`, payload)).data as CuratorReviewResponse;
+  return res.data;
+};
+
+export const putCuratorReview = async (payload: PutCuratorReviewPayload, id: number) => {
+  const res = (await api.put(`${path}/${id}`, payload)).data as CuratorReviewResponse;
+  return res.data;
+};
+
+export const deleteCuratorReview = async (id: number) => {
+  const res = (await api.delete(`${path}/${id}`)).data as CuratorReviewResponse;
+  return res.data;
+};
