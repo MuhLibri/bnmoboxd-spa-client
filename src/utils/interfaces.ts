c02975da6cb9a65dae60e94421ae035e786b86eb
chaerla <chelgchen@gmail.com>
2023-11-01 23:11:23 +0700

    chore: add interfaces

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -8,4 +8,26 @@ export interface User {
   firstName: string;
   lastName?: string;
   email: string;
+  isAdmin: boolean;
 }
+
+export interface CuratorReview {
+  id?: number;
+  filmId: number;
+  review: string;
+  rating: number;
+  updatedAt?: string;
+}
+
+export interface Token {
+  exp: string;
+  iap: string;
+  user: User;
+}
+
+export interface Film {
+  id: number;
+  title: string;
+}
+
+export type SUBSCRIPTION_STATUS = 'PENDING' | 'ACCEPTED' | 'REJECTED';
