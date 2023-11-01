e0b6ca990860feec76e0fe09202cedfa497736f9
chaerla <chelgchen@gmail.com>
2023-11-01 23:08:30 +0700

    feat: add token storage

--- src/utils/token-storage.ts
+++ src/utils/token-storage.ts
@@ -1,11 +1,21 @@
-export function getToken() {
+export const getToken = () => {
   return localStorage.getItem('token');
-}
+};
 
-export function setToken(token: string | null) {
+export const setToken = (token: string | null) => {
   if (token) {
     localStorage.setItem('token', token);
+    localStorage.setItem('isAdmin', 'false');
   } else {
     localStorage.removeItem('token');
   }
-}
+};
+
+// export const getRole = (token: string) => {
+//   const res = useJwt(token);
+//
+//   // const user = res.user as User;
+//   // if (isExpired || !user?.isAdmin) {
+//   //   return <Navigate to="/login" />;
+//   // }
+// };
