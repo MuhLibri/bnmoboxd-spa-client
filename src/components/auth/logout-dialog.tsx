4c4a1b6e3f261249f7bdd89a6de33e7416f9db81
chaerla <chelgchen@gmail.com>
2023-11-01 23:15:37 +0700

    feat: add logout dialog

---
+++ src/components/auth/logout-dialog.tsx
@@ -0,0 +1,45 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button.tsx';
+
+import { useState } from 'react';
+import { setToken } from '@/utils/token-storage.ts';
+import { useNavigate } from 'react-router-dom';
+
+export const LogoutDialog = () => {
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline" className="lg:w-full">
+          Logout
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure you want to logout?</AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => {
+              setToken(null);
+              navigate('/login');
+            }}
+          >
+            Yes
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
