85ecd095e3c54608a0e71588b29a630eb4cb95fb
chaerla <chelgchen@gmail.com>
2023-11-09 00:43:11 +0700

    refactor: removed public from logo path

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="lg:grid lg:grid-cols-6 w-full min-h-screen border-box">
       <Card className="p-8 space-y-8">
-        <img src="public/logo.png" alt="logo" className="mx-auto w-[150px]" />
+        <img src="/logo.png" alt="logo" className="mx-auto w-[150px]" />
       </Card>
       <div className="lg:col-span-5 px-8 py-12 md:p-20 lg:py-20 space-y-4">{children}</div>
     </div>
