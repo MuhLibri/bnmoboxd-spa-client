099b30fc02aefedc1d2790fe4c3e7a195e2b6bf9
chaerla <chelgchen@gmail.com>
2023-11-10 23:31:57 +0700

    feat: add subscriptions services

---
+++ src/services/subscriptions.ts
@@ -0,0 +1,23 @@
+import { api } from '@/utils/api.ts';
+import { Subscription, SUBSCRIPTION_STATUS } from '@/utils/interfaces.ts';
+
+const path = '/subscription';
+
+export interface SubscriptionsResponse {
+  data: { subscriptions: Subscription[]; count: number };
+}
+
+export interface ManageSubscriptionPayload {
+  curatorUsername: string;
+  subscriberUsername: string;
+  status: SUBSCRIPTION_STATUS;
+}
+export const getSubscriptions = async ({ page, take }: { page: number; take: number }) => {
+  const res = (await api.get(`${path}?page=${page}&take=${take}`)).data as SubscriptionsResponse;
+  return res.data;
+};
+
+export const manageSubscription = async (payload: ManageSubscriptionPayload) => {
+  const res = (await api.put(`${path}`, payload)).data as SubscriptionsResponse;
+  return res.data;
+};
