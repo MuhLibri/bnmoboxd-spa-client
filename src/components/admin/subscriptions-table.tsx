ca35407f01e86099dede48352335e45a1d0832e7
chaerla <chelgchen@gmail.com>
2023-11-10 23:32:12 +0700

    feat: integrated subscription table with backend

--- src/components/admin/subscriptions-table.tsx
+++ src/components/admin/subscriptions-table.tsx
@@ -1,34 +1,20 @@
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table.tsx';
 import { SubscriptionBadge } from '@/components/admin/subscription-badge.tsx';
 import { AcceptDialog, RejectDialog } from '@/components/admin/manage-subscriptions-dialog.tsx';
+import { useQuery } from '@tanstack/react-query';
+import { getSubscriptions } from '@/services/subscriptions.ts';
+import { Loader2 } from 'lucide-react';
+import { useSearchParams } from 'react-router-dom';
+import { Pagination } from '@/components/ui/pagination.tsx';
 
 export const SubscriptionsTable = () => {
-  const subscriptions = [
-    {
-      curatorUsername: 'curator1',
-      subscriberUsername: 'subscriber2',
-      status: 'PENDING',
-    },
-    {
-      curatorUsername: 'curator1',
-      subscriberUsername: 'subscriber1',
-      status: 'ACCEPTED',
-    },
-    {
-      curatorUsername: 'curator1',
-      subscriberUsername: 'subscriber3',
-      status: 'REJECTED',
-    },
-    {
-      curatorUsername: 'curator1',
-      subscriberUsername: 'subscriber4',
-      status: 'PENDING',
-    },
-  ];
+  const [searchParams] = useSearchParams();
+  const page = parseInt(searchParams.get('page') || '1');
+  const { data, isLoading } = useQuery({ queryKey: ['subscriptions', page], queryFn: () => getSubscriptions({ page, take: 9 }) });
 
   return (
     <Table className="w-full">
-      <TableCaption>Page 1 of 100</TableCaption>
+      <TableCaption>{data && data.count > 0 && <Pagination totalItems={data.count || 0} take={10} />}</TableCaption>
       <TableHeader>
         <TableRow>
           <TableHead>User</TableHead>
@@ -38,7 +24,12 @@ export const SubscriptionsTable = () => {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {subscriptions.map((sub, index) => (
+        {isLoading && (
+          <div className="w-full flex items-center justify-center">
+            <Loader2 className="mr-2 h-12 w-12 animate-spin" />
+          </div>
+        )}
+        {data?.subscriptions.map((sub, index) => (
           <TableRow key={index}>
             <TableCell>{sub.subscriberUsername}</TableCell>
             <TableCell>{sub.curatorUsername}</TableCell>
@@ -47,8 +38,8 @@ export const SubscriptionsTable = () => {
             </TableCell>
             <TableCell>
               <div className="flex flex-row items-center space-x-4">
-                <AcceptDialog />
-                <RejectDialog />
+                <AcceptDialog subscription={sub} />
+                <RejectDialog subscription={sub} />
               </div>
             </TableCell>
           </TableRow>
