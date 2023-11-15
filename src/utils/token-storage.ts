85e9fb1b6e6a3a628448490a07bd49de6d589666
chaerla <chelgchen@gmail.com>
2023-11-15 13:36:00 +0700

    chore: removed comments

--- src/utils/token-storage.ts
+++ src/utils/token-storage.ts
@@ -5,17 +5,7 @@ export const getToken = () => {
 export const setToken = (token: string | null) => {
   if (token) {
     localStorage.setItem('token', token);
-    localStorage.setItem('isAdmin', 'false');
   } else {
     localStorage.removeItem('token');
   }
 };
-
-// export const getRole = (token: string) => {
-//   const res = useJwt(token);
-//
-//   // const user = res.user as User;
-//   // if (isExpired || !user?.isAdmin) {
-//   //   return <Navigate to="/login" />;
-//   // }
-// };
